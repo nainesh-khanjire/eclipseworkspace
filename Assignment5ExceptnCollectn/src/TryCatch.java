bb80799b2f116f7854a8841b8ed5211b163792cb
Nainesh Khanjire <nkhanjire@gmail.com>
2022-10-16 09:02:50 +0530

    update

--- Assignment5ExceptnCollectn/src/TryCatch.java
+++ Assignment5ExceptnCollectn/src/TryCatch.java
@@ -1,8 +1,9 @@
 
 public class TryCatch {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	
+	{
 		/*
 		 * 1) Write a program to demonstrate the use of try, catch, finally throw and
 		 * throws keywords and demonstrate the following points in the program. a)
@@ -19,8 +20,8 @@ public class TryCatch {
 
 			int c = a / b; // this creates error
 
-		} catch (ArithmeticException e) {
-			System.out.println("Arithmetic exception displayed: " + e); // displays error
+		} catch (ArithmeticException v) {
+			System.out.println("Arithmetic exception displayed: " + v); // displays error
 
 		} catch (ArrayIndexOutOfBoundsException i) {
 			System.out.println("array exception displayed :" + i);
@@ -33,15 +34,57 @@ public class TryCatch {
 		System.out.println("\n\nb) try-catch-finally combination >>");
 		// b) try-catch-finally combination
 		try {
+			int a = 50;
+			int d = 0;
+			// int c1 = a / d; // error expected, uncomment before running b)
 
-		} catch (ArithmeticException a) {
-
+		} catch (NullPointerException a) { // used wrong exception type, creates error
+			System.out.println(a);
+		} finally {
+			System.out.println("this finally block will get executed regardless of the errors");
 		}
+		System.out.println("this is rest of the code"); // default print stmt gets printeds
+
+		System.out.println("\n\nc) try-finally combination >>");
 
 		// c) try-finally combination
+		try {
+			int a = 40;
+			int e = 0;
+			// int c2 = a / e; // error expected , uncomment before running this block
+
+		} finally {
+
+			System.out.println("this is try finally block that will get executed");
+		}
+
+		System.out.println("\n\nd) try-nested block >>");
 
 		// d) Nested try blocks
 
-	}
+		try {
+			int p = 30;
+			int q = 0;
+			int r = 0;
+			int s = 0;
 
-}
+			int t = p / q; // error 1
+			try {
+				int u = p / r; // error 2
+				try {
+					int y = p / s; // error 3
+
+				} catch (ArithmeticException a) {
+					System.out.println(a);
+
+				}
+			} catch (ArithmeticException n) {
+				System.out.println(n);
+
+			}
+		} catch (ArithmeticException m) {
+
+			System.out.println(m);
+
+		}}
+	}
